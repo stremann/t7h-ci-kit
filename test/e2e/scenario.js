40ba550aa339c89f915a398ee3bde39ed3f9b407
Andrey Kolesnik <andreas.kolesnik@gmail.com>
2016-04-01 11:19:43 +0300

    Add sauce

---
+++ test/e2e/scenario.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    afterEach(function () {
+        //helper.checkConsoleLog();
+    });
+
+
+
+
+    describe('Navigation', function () {
+
+            it('should start on the "LoanTradeSettlement" main page', function () {
+                browser.get('/');
+                expect(browser.getTitle()).toEqual('Travel and help');
+            });
+    });
+
+
+
+})();
