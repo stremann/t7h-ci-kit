35ce571152739fc00457150be43d66b354550eed
Andrey Kolesnik <andreas.kolesnik@gmail.com>
2016-04-01 11:52:38 +0300

    add browser.ignoreSynchronization = true;

--- test/e2e/scenario.js
+++ test/e2e/scenario.js
@@ -11,6 +11,7 @@
     describe('Navigation', function () {
 
             it('should start on the "LoanTradeSettlement" main page', function () {
+                browser.ignoreSynchronization = true;
                 browser.get('/');
                 expect(browser.getTitle()).toEqual('Travel and help');
             });
